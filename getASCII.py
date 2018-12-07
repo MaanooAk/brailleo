24ad34eba5bb577d1b3027a4552ce59b72a79d51
xlxs4 <oorestis@csd.auth.gr>
2018-12-07 12:15:39 +0200

    typo fix

--- getASCII.py
+++ getASCII.py
@@ -3,6 +3,7 @@ import getch
 #prints out braille characters's correspondant ASCII code equivalents
 string = "⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
 for char in string:
+  print(char)
   print(ord(char))
 print("every keypress will return it's correspondant ASCII code\npress esc to exit")
 while True:
